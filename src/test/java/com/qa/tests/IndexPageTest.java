cfe7fa2821b6b54d73b21ced6b756e3d5b3e83b5
vishwpatel511 <vishwpatel511@gmail.com>
2021-07-25 14:58:34 -0400

    fixed class can not be initiated error

--- src/test/java/com/qa/tests/IndexPageTest.java
+++ src/test/java/com/qa/tests/IndexPageTest.java
@@ -18,7 +18,7 @@ import org.testng.annotations.AfterMethod;
 
 public class IndexPageTest extends TestBase {
 	
-	IndexPage indexpage = new IndexPage();
+	IndexPage indexpage;
 	
 	public IndexPageTest() {
 		super();
@@ -33,14 +33,14 @@ public class IndexPageTest extends TestBase {
    
    @Test(priority=1)
    public void PageTitleTest() {
-	   
+	   IndexPage indexpage = new IndexPage();
 	   Assert.assertEquals(indexpage.VerifyIndexPageTitle(), "STORE", "Title is not as expected");
 	   
    }
    
    @Test(priority=2)
    public void LinksPresentTest() {
-	 
+	   IndexPage indexpage = new IndexPage();
 	 String[] expectedLinksArray =new String[] {"Home ", "Contact", "About us", "Cart", "Log in", "Sign up"};  
 	
 	 for(int i =0; i<expectedLinksArray.length; i++) {
@@ -52,7 +52,7 @@ public class IndexPageTest extends TestBase {
    
    @Test(priority=3)
    public void LoginFunctionalityTest() throws InterruptedException {
-	   
+	   IndexPage indexpage = new IndexPage();
 	   indexpage.VerifyLoginFunctionality(TestBase.prop.getProperty("username"), TestBase.prop.getProperty("password"));
 	   Thread.sleep(2000);
 	   Assert.assertEquals(TestBase.driver.findElement(By.xpath("//div[@id='navbarExample']/ul/li[6]/a")).getText(), "Log out");
@@ -60,6 +60,7 @@ public class IndexPageTest extends TestBase {
    
    @Test(priority=4)
    public void AboutUsFooterTest() {
+	   IndexPage indexpage = new IndexPage();
 	   Assert.assertTrue(indexpage.VerifyAboutUsFooter().contains("We believe performance needs to be validated"));
 	   
    }
