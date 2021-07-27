06921680d9e167015db052fdf4aabf60d4040c40
vishwpatel511 <vishwpatel511@gmail.com>
2021-07-27 17:06:22 -0400

    generated extent-report using extent-report class

---
+++ src/main/java/com/qa/utils/ScreenShotOnFailedTests.java
@@ -0,0 +1,13 @@
+package com.qa.utils;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.qa.base.TestBase;
+
+public class ScreenShotOnFailedTests extends TestBase{
+	
+	
+}
